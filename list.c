209e166e0206692faf50ce939db702387052ff4f
Azzahrah <joko.pitoyo@gmail.com>
2017-03-29 08:48:56 +0700

    Initial Commit

--- list.c
+++ list.c
@@ -3,5 +3,10 @@
 2. RTMP Encoder
    https://github.com/begeekmyfriend/yasea	
 3. Video Streaming
-   https://github.com/fyhertz
+   https://github.com/fyhertz  
+4. IPTV Hardware
+   tbsiptv.com
+5. MiniPC with PCIExpress
+   https://nodka1.en.alibaba.com/?spm=a2700.8239084.0.0.xWJHIe
+   https://www.alibaba.com/product-detail/industrial-mini-pc-with-2-PCI_1561022390.html
    
