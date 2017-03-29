a8bcc6816072b7126ad594e7c1ab773471e4beb6
Azzahrah <joko.pitoyo@gmail.com>
2017-03-29 09:47:35 +0700

    Initial Commit

--- list.c
+++ list.c
@@ -10,3 +10,15 @@
    https://nodka1.en.alibaba.com/?spm=a2700.8239084.0.0.xWJHIe
    https://www.alibaba.com/product-detail/industrial-mini-pc-with-2-PCI_1561022390.html
    
+6. Websocket Opensource
+   https://github.com/andremussche/DelphiWebsockets
+   
+7. Setting IP Static Raspberry PI 
+   pico /etc/dhcpcd.conf
+   ---  at the end of file, add following statement, adjust eth0 device to actual device, adjust ip address, network, route and doamin name servers
+   
+   interface eth0
+   static ip_address=192.168.1.3/24
+   static routers=192.168.1.1
+   static domain_name_servers=192.168.1.1 8.8.8.8
+ 
